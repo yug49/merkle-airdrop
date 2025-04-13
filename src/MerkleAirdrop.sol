18f4423ea750b20b881e44ff8b59377c5c453ce5
yug49 <148035793+yug49@users.noreply.github.com>
2025-04-14 01:37:43 +0530

    Random Token

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {RandomToken} from "./RandomToken.sol";
+
+contract MerkleAirdrop {
+    // some list of addresses
+    // allow someone in the list to claim ERC-20 tokens
+}
---
+++ src/RandomToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol"; 
+
+contract RandomToken is ERC20, Ownable{
+    constructor() ERC20("RandomToken", "RND") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
